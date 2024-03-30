17be650003a5af55bc4aa74128a41e31d16bbc70
StarLord <37327630+Adishks101@users.noreply.github.com>
2024-03-30 19:49:13 +0530

    feature: SignIn Added Sign In feature with jwt token return.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -26,6 +27,34 @@ export const signUp = async (req, res, next) => {
     try {
       await user.save();
       res.json("created successfully.");
+    } catch (error) {
+      next(error);
+    }
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(errorHandler(400, "All fields are required."));
+  } else {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        next(errorHandler(400, "Invalid Credentials."));
+      } else {
+        const { password: pass, ...userDetails } = user._doc;
+        const isMatch = bcryptjs.compareSync(password, pass);
+        if (isMatch) {
+          const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+          res
+            .status(200)
+            .cookie("access_token", token, { httpOnly: true })
+            .json(userDetails);
+        } else {
+          next(errorHandler(400, "Invalid Credentials."));
+        }
+      }
     } catch (error) {
       next(error);
     }
